d41b935009a256cab8ee1b74ff8bd06e6c486e82
Doug Sweetser <sweetser@alum.mit.edu>
2020-03-08 14:18:53 -0400

    Updated.

--- webapp__rotations_only.py
+++ webapp__rotations_only.py
@@ -17,11 +17,11 @@ from Qs import (
 
 
 # Sidebar setup.
-Rodrigues_func = st.sidebar.checkbox(label="h q h^-1", value=True)
-Rodrigues_generalized_func = st.sidebar.checkbox(
-    label="h'=vec(h)/|vec(h)|; h' q h'* + 1/2((h' h' q)* - (h'* h'* q)*)", value=True
-)
-
+Rodrigues_func = st.sidebar.checkbox(label="h q h⁻¹", value=True)
+Rodrigues_generalized_func = st.sidebar.checkbox(label="Rotation only", value=True)
+st.sidebar.markdown("Rotation only function:")
+image = Image.open("images/Rodrigues_generalization.only_rotation.2_lines.png")
+st.sidebar.image(image)
 # U_zero = st.sidebar.radio(label="Make one zero", options=("U.t", "U.x", "U.y", "U.z"))
 the_fix = st.sidebar.radio(label="U is", options=("fixed", "random"), index=1)
 zero_t = st.sidebar.checkbox(label="Zero t", value=False)
@@ -92,7 +92,7 @@ if Rodrigues_func:
                 "y": Rodrigues_data.df[2],
                 "z": Rodrigues_data.df[3],
             },
-            name="Rotation",
+            name="h U h⁻¹",
             mode="markers",
             marker=dict(size=POINT_SIZE, opacity=OPACITY, color="violet"),
         )
@@ -106,7 +106,7 @@ if Rodrigues_generalized_func:
                 "y": Rodrigues_generalized_data.df[2],
                 "z": Rodrigues_generalized_data.df[3],
             },
-            name="Generalized rotation",
+            name="Rotation only",
             mode="markers",
             marker=dict(size=POINT_SIZE, opacity=OPACITY, color="springgreen"),
         )
@@ -149,25 +149,44 @@ st.markdown(f"{table}")
 
 st.markdown(
     """### Discussion
-The same quaternion, q=(1, 1, 2, 3) is the start point. It has a quaternion norm squared of (15, 0, 0, 0). The squared
-value is (-13, 2, 4, 6). Two rotation functions can be used. The first is the rotation function developed by Rodrigues
-in 1843. This is applied over and over again, the number of times determined by the dimension value. There is also
-a choice for the quaternion _h_. It is either chosen randomly or can be fixed at a value of h=(2, 3, 2, 1).
+The same quaternion, q=(1, 1, 2, 3), is the start point for all graphs. It has a quaternion norm squared of 
+(15, 0, 0, 0). The squared value is (-13, 2, 4, 6). Two rotation functions can be used. The first is the Rodrigues 
+rotation function developed by Olinde Rodrigues in 1843. This is applied over and over again, the number of times 
+determined by the dimension value. There is also a choice for the quaternion _h_. It is either chosen randomly or can be 
+fixed at a value of h=(2, 3, 2, 1).
 
-The second function was proposed to do Lorentz boosts by D. Sweetser in 2010 and independently by Dr. M. Kharinov (year 
-not known). When initially found, it was not realized that rotations were possible! A mere 3 years later, it was noticed 
-that if the first term of the parameter _h_ was zero and its norm was unity, a 3D rotation would result.
+The second function I call the Rodrigues generalized rotation function was proposed to do Lorentz boosts by D. Sweetser 
+in 2010 and independently by Dr. M. Kharinov (year not known). When initially found, it was not realized that 3D
+rotations were possible! A mere 3 years later, it was noticed that if the first term of the parameter _h_ was zero and 
+its norm was unity, a 3D rotation would result.
 
 The function written on one line is incorrect. Only in the Spring of 2020 have I begun to write the Rodrigues 
-generalization as a two part function like so:
+generalized rotation function as a two-part function like so:
 """
 )
 image = Image.open("images/Rodrigues_generalization_multi-part.gif")
 st.image(image)
 st.markdown(
-    """The first part does rotations. The second part does boosts. The two parts divide the full set of
-quaternions into to parts: those with a scalar equal to zero and those with a non-zero scalar. Each part has three
-degrees of freedom, for a total of six as necessary to represent the Lorentz group."""
+    """The first part does rotations. When the parameter _h_ has a first term equal to zero, then the parameter is
+normalized to one. The second part does boosts. This time the first term is used to form a hyberbolic cosine and sine
+function so that its square is equal to one. The two parts of the function divide the full set of quaternions into two 
+non-intersecting sets: those with a first term equal to zero and those with a non-zero first term. Each part of the 
+function has three degrees of freedom, for a total of six as necessary to represent the full Lorentz group.
+
+For this webapp, the only the first part of the Rodrigues generalized rotation function is use. This can be done by
+calling the function with another function that passes a zero for the first term and the three vector terms.
+
+The Rodrigues rotation and Rodrigues generalized rotation function are distinct. The Rodrigues rotation function uses
+both real and imaginary parts of a quaternion to do a rotation. The Rodrigues generalized rotation function is 
+restricted to do rotations using only the real part. The difference between the two functions can be illustrated by 
+clicking the 'fixed' button. 
+
+It would be wrong to conclude that there are some rotations that are not possible using either the Rodrigues function
+or the Rodrigues generalized rotation function. The fixed calculation is a fixed value for _q_ and a fixed value for 
+_h_. It can be shown analytically that for any rotation _q_ to _q'_, there is a _h_ that can be chosen to do the task.
+
+The code used to generate the webapp and the math behind the functions can be inspected by clicking the 'show code'
+button. The site can be cloned from """
 )
 
 
