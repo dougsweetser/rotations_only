e0a1f9e66abd3a4cf788fbdddeb905b3fb3e2162
Doug Sweetser <sweetser@alum.mit.edu>
2020-03-07 21:53:48 -0500

    Changed default state, added Discussion.

--- webapp__rotations_only.py
+++ webapp__rotations_only.py
@@ -1,119 +1,103 @@
 import streamlit as st
 import plotly.graph_objects as go
 import textwrap
-import pandas as pd
+from PIL import Image
 
-from Qs import Q, Qs, qrandom, rotation, rotation_only, generate_QQs
-
-
-def zero_out(
-    q_1: Q, t: bool = False, x: bool = False, y: bool = False, z: bool = False
-):
-    """
-    Puts a zero in one or more of the four places.
-    """
-
-    if t:
-        q_1.t = 0
-
-    if x:
-        q_1.x = 0
-
-    if y:
-        q_1.y = 0
-
-    if z:
-        q_1.z = 0
-
-    return q_1
-
-
-def zero_outs(
-    q_1: Qs, t: bool = False, x: bool = False, y: bool = False, z: bool = False
-):
-    """
-    Puts a zero in one or more of the four places.
-    """
-    return Qs(
-        [zero_out(q, t, x, y, z) for q in q_1.qs],
-        qs_type=q_1.qs_type,
-        rows=q_1.rows,
-        columns=q_1.columns,
-    )
+from Qs import (
+    Q,
+    Qs,
+    qrandom,
+    rotation,
+    squares,
+    norm_squareds,
+    rotation_only,
+    generate_QQs,
+    zero_out,
+    zero_outs,
+)
 
 
 # Sidebar setup.
 Rodrigues_func = st.sidebar.checkbox(label="UqU^-1", value=True)
 Rodrigues_generalized_func = st.sidebar.checkbox(
-    label="UqU* + 1/2((U U q)* - (U* U* q)*)", value=False
+    label="UqU* + 1/2((U U q)* - (U* U* q)*), U.t = 0", value=True
 )
 
 # U_zero = st.sidebar.radio(label="Make one zero", options=("U.t", "U.x", "U.y", "U.z"))
-the_fix = st.sidebar.radio(label="U is", options=("fixed", "random"))
+the_fix = st.sidebar.radio(label="U is", options=("fixed", "random"), index=1)
 zero_t = st.sidebar.checkbox(label="Zero t", value=False)
 zero_x = st.sidebar.checkbox(label="Zero x", value=False)
 zero_y = st.sidebar.checkbox(label="Zero y", value=False)
 zero_z = st.sidebar.checkbox(label="Zero z", value=False)
-dim = st.sidebar.slider(label="Dimensions", min_value=10, max_value=500, value=100)
+dim = st.sidebar.slider(label="Dimensions", min_value=10, max_value=500, value=75)
 
 show_code = st.sidebar.checkbox("Show code", False)
 
 # The calculation - make a DataFrame
+initial_point = Q([1, 1, 2, 3])
+fixed_point = Q([2, 3, 2, 1])
 
 if the_fix == "fixed":
-    Rodrigues_data = pd.DataFrame(
-        zero_outs(
-            generate_QQs(rotation, Q([1, 1, 2, 3]), Q([0, 3, 2, 1]), dim=dim),
-            zero_t,
-            zero_x,
-            zero_y,
-            zero_z,
-        ).xyz()
+    Rodrigues_data = zero_outs(
+        generate_QQs(rotation, initial_point, fixed_point, dim=dim),
+        zero_t,
+        zero_x,
+        zero_y,
+        zero_z,
     )
-    Rodrigues_generalized_data = pd.DataFrame(
-        zero_outs(
-            generate_QQs(rotation_only, Q([1, 1, 2, 3]), Q([2, 3, 2, 1]), dim=dim),
-            zero_t,
-            zero_x,
-            zero_y,
-            zero_z,
-        ).xyz()
+    Rodrigues_generalized_data = zero_outs(
+        generate_QQs(rotation_only, initial_point, fixed_point, dim=dim),
+        zero_t,
+        zero_x,
+        zero_y,
+        zero_z,
     )
 
 elif the_fix == "random":
-    Rodrigues_data = pd.DataFrame(
-        zero_outs(
-            generate_QQs(rotation, Q([1, 1, 2, 3]), qrandom, dim=dim),
-            zero_t,
-            zero_x,
-            zero_y,
-            zero_z,
-        ).xyz()
+    Rodrigues_data = zero_outs(
+        generate_QQs(rotation, initial_point, qrandom, dim=dim),
+        zero_t,
+        zero_x,
+        zero_y,
+        zero_z,
     )
-    Rodrigues_generalized_data = pd.DataFrame(
-        zero_outs(
-            generate_QQs(rotation_only, Q([1, 1, 2, 3]), qrandom, dim=dim),
-            zero_t,
-            zero_x,
-            zero_y,
-            zero_z,
-        ).xyz()
+    Rodrigues_generalized_data = zero_outs(
+        generate_QQs(rotation_only, initial_point, qrandom, dim=dim),
+        zero_t,
+        zero_x,
+        zero_y,
+        zero_z,
     )
 
+# collect stats
+Rodrigues_data_squares = squares(Rodrigues_data)
+Rodrigues_data_norm_squares = norm_squareds(Rodrigues_data)
+
+Rodrigues_generalized_data_squares = squares(Rodrigues_generalized_data)
+Rodrigues_generalized_data_norm_squares = norm_squareds(Rodrigues_generalized_data)
 
 # Main page.
+
+# st.markdown("<style='background-color:powderblue;'></style>", unsafe_allow_html=True),
 
 st.title("Rotations Only")
 
 fig = go.Figure()
-go.Layout(margin=dict(t=500))
+go.Layout()
+POINT_SIZE = 6
+OPACITY = 0.5
+
 if Rodrigues_func:
     fig.add_trace(
         go.Scatter3d(
-            {"x": Rodrigues_data[0], "y": Rodrigues_data[1], "z": Rodrigues_data[2]},
-            name="rotation",
+            {
+                "x": Rodrigues_data.df[1],
+                "y": Rodrigues_data.df[2],
+                "z": Rodrigues_data.df[3],
+            },
+            name="Rotation",
             mode="markers",
-            marker=dict(size=4, opacity=0.5),
+            marker=dict(size=POINT_SIZE, opacity=OPACITY, color="violet"),
         )
     )
 
@@ -121,26 +105,75 @@ if Rodrigues_generalized_func:
     fig.add_trace(
         go.Scatter3d(
             {
-                "x": Rodrigues_generalized_data[0],
-                "y": Rodrigues_generalized_data[1],
-                "z": Rodrigues_generalized_data[2],
+                "x": Rodrigues_generalized_data.df[1],
+                "y": Rodrigues_generalized_data.df[2],
+                "z": Rodrigues_generalized_data.df[3],
             },
             name="Generalized rotation",
             mode="markers",
-            marker=dict(size=4, opacity=0.5),
+            marker=dict(size=POINT_SIZE, opacity=OPACITY, color="springgreen"),
         )
     )
-fig.update_layout(margin=dict(l=20, r=20, b=20, t=20))
+
+initial_point_zeroed = zero_out(initial_point, x=zero_x, y=zero_y, z=zero_z)
+ipz_x, ipz_y, ipz_z = (
+    initial_point_zeroed.x,
+    initial_point_zeroed.y,
+    initial_point_zeroed.z,
+)
+fig.add_trace(
+    go.Scatter3d(
+        {"x": [ipz_x], "y": [ipz_y], "z": [ipz_z]},
+        name=f"Initial point: {ipz_x}, {ipz_y}, {ipz_z}",
+        mode="markers",
+        marker=dict(size=POINT_SIZE, opacity=OPACITY, color="red"),
+    )
+)
+
 st.write(fig)
-# st.markdown("## Norm Squared Values")
-#
-# total = norm_squared(prod).t
-# odd = norm_squared(cx_origin).t
-#
-# table = f"""Odd | Total
-# --- | --- | ---
-# {odd} | {total}"""
-# st.markdown(f"{table}")
+
+st.markdown("### Norm Squared and Squared Values")
+
+table = f"""t² + R² | t²-R² | 2 t x | 2 t y | 2 t z
+--- | --- | --- | -- | --
+"""
+
+if Rodrigues_func:
+    means = Rodrigues_data_squares.df.mean()
+    square = Rodrigues_data_norm_squares.df[0] / dim
+    table += f"{square[0]:.2f} | {means[0]:.2f} | {means[1]:.2f} | {means[2]:.2f} | {means[3]:.2f}\n"
+
+if Rodrigues_generalized_func:
+    means = Rodrigues_generalized_data_squares.df.mean()
+    square = Rodrigues_generalized_data_norm_squares.df[0] / dim
+    table += f"{square[0]:.2f} | {means[0]:.2f} | {means[1]:.2f} | {means[2]:.2f} | {means[3]:.2f}"
+
+st.markdown(f"{table}")
+
+st.markdown(
+    """### Discussion
+The same quaternion, q=(1, 1, 2, 3) is the start point. It has a quaternion norm squared of (15, 0, 0, 0). The squared
+value is (-13, 2, 4, 6). Two rotation functions can be used. The first is the rotation function developed by Rodrigues
+in 1843. This is applied over and over again, the number of times determined by the dimension value. There is also
+a choice for the quaternion _h_. It is either chosen randomly or can be fixed at a value of h=(2, 3, 2, 1).
+
+The second function was proposed to do Lorentz boosts by D. Sweetser in 2010 and independently by Dr. Kharinov (year not 
+known). WHen initially used, it was not realized that rotations were possible! A mere 3 years later, it was noticed that 
+if the first term of the parameter _U_ was zero and the norm of _h_ was 
+unity, a normal 3D rotation would result.
+
+The function written as I had on one line never passed a sniff test - it looks like it can only have at most three
+degrees of freedom yet the Lorentz group requires six, three for rotations, three for boosts. Only in the Spring of
+2020 have I begun to write the Rodrigues generalation as a two part function like so:
+"""
+)
+image = Image.open("Rodrigues_generalization_multi-part.gif")
+st.image(image)
+st.markdown(
+    """The first part does rotations. The second part does boosts. The two parts divide the full set of
+quaternions into to parts: those with a scalar equal to zero and those with a non-zero scalar. Each part has three
+degrees of freedom, for a total of six as necessary to represent the Lorentz group."""
+)
 
 
 def show_file(label: st, file_name: str, code: bool = False):
